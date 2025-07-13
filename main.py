d9cf0961457c61704786b39b66dd3b8ea4b5c4b8
Trebor <robiafarczad@gmail.com>
2025-07-13 22:39:00 +0300

    Added cycling animations, cli params soon to come

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import subprocess
+import time as t
 
 def get_neofetch_data():
     neofetch_command = ["neofetch", "--off"]
@@ -15,14 +16,40 @@ def get_anim_frame(index):
     lines = [line.ljust(longest_line_length) for line in lines]
     return lines
 
+def get_anim_frames(lower, upper):
+    frames = []
+    for i in range(lower, upper + 1):
+        frames.append(get_anim_frame(i))
+    return frames
+
+def get_full_frames(anim_frames, specs):
+    longest_line_length = max([len(line) for frame in anim_frames for line in frame])
+    frames = []
+    for anim_frame in anim_frames:
+        # padd the frame if needed
+        anim_frame = [line.ljust(longest_line_length) for line in anim_frame]
+        frame = []
+        for i in range(max(len(anim_frame), len(specs))):
+            anim_line = anim_frame[i] if i < len(anim_frame) else " " * longest_line_length
+            spec_line = specs[i] if i < len(specs) else " " * longest_line_length
+            frame.append(anim_line + "  " + spec_line)
+
+        frames.append(frame)
+
+    return frames
 
 def main():
-    specs = get_neofetch_data()
-    frame = get_anim_frame(1)
+    frames = get_full_frames(get_anim_frames(1, 2), get_neofetch_data())
 
-    for i in range(max(len(specs), len(frame))):
-        spec_line = specs[i] if i < len(specs) else ""
-        frame_line = frame[i] if i < len(frame) else " " * len(frame[-1])
-        print(f"{frame_line}  {spec_line}")
+    fps = 2
+    time = None
+    frame_num = len(frames)
+    current_frame = 0
+    while True:
+        # clear the screen
+        print("\033[H\033[J", end="")
+        print("\n".join(frames[current_frame]))
+        current_frame = (current_frame + 1) % frame_num
+        t.sleep(1 / fps)
 
 main()
